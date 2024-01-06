b2d44f63fd163f491a1b66b53e9a62bc684d659b
valentina buoro <valentinabuoro4@gmail.com>
2024-01-06 19:46:51 +0100

    qr generator

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
-console.log('Hello World!');
+import inquirer from 'inquirer';
+import fs from 'fs';
+import qr from 'qr-image';
+
+
+inquirer
+  .prompt([
+    {
+        message: "Enter a URL to generate a QR code",
+        name: "URL",
+    }
+    /* Pass your questions in here */
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qr_image.png'));
+
+    fs.writeFile('url.txt', url, (err) => {
+    
+    if (err) throw err;
+    console.log('The QR code has been saved!');
+
+  })})
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
 /* 
 1. use the inquirer npm package to get user input
 2. use the qr-image npm package to generate a QR code image with the entered url
