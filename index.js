78597a8baa502ee7cbe7babfcc0483b3d97c8465
valentina buoro <valentinabuoro4@gmail.com>
2024-01-06 19:24:43 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log('Hello World!');
+/* 
+1. use the inquirer npm package to get user input
+2. use the qr-image npm package to generate a QR code image with the entered url
+3. create a txt file to save the user input using the native fs module
+*/
